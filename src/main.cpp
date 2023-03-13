0fbc769fda3b8cda538f6dbc9f15f235ba8cfb49
Liam Sho <conan_creeper@outlook.comexport>
2023-03-13 20:58:25 +0800

    feat: init logger

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,39 @@
  */
 
 #include <iostream>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+
+void logger_init();
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    logger_init();
+
+    spdlog::info("Starting 3d_reconstruction");
+
     return 0;
+}
+
+void logger_init() {
+    try {
+        auto console_sink =
+            std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        console_sink->set_level(spdlog::level::debug);
+
+        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
+            "logs/log.txt", true);
+        file_sink->set_level(spdlog::level::trace);
+
+        spdlog::sinks_init_list sink_list = {file_sink, console_sink};
+
+        spdlog::logger logger("default", sink_list.begin(), sink_list.end());
+        logger.set_level(spdlog::level::debug);
+
+        spdlog::set_default_logger(std::make_shared<spdlog::logger>(
+            "default", spdlog::sinks_init_list({console_sink, file_sink})));
+
+    } catch (const spdlog::spdlog_ex &ex) {
+        std::cout << "Log initialization failed: " << ex.what() << std::endl;
+    }
 }
