3b1b7eb00fb6ce23a4f6430ad70e467f49880e27
LiamSho <conan_creeper@outlook.com>
2023-03-13 19:22:27 +0800

    chore: repo init

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2023 Alisa.
+ *
+ * This file is part of 3d_reconstruction.
+ *
+ * 3d_reconstruction is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * 3d_reconstruction is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
